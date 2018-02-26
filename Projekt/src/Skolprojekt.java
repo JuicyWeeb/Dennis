f5763f2d5c50d351f483bae89dcf4d8f643fc763
JuicyWeeb <36629563+JuicyWeeb@users.noreply.github.com>
2018-02-26 09:44:13 +0100

    Add files via upload

--- Projekt/src/Skolprojekt.java
+++ Projekt/src/Skolprojekt.java
@@ -26,21 +26,320 @@ public class Skolprojekt extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        pnlStenSaxPase = new javax.swing.JPanel();
+        btnSten = new javax.swing.JButton();
+        btnSax = new javax.swing.JButton();
+        btwPase = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        txfResultat = new javax.swing.JTextPane();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        txfDatornsval = new javax.swing.JTextPane();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        txfDittVal = new javax.swing.JTextPane();
+        jLabel2 = new javax.swing.JLabel();
+        btnRestart = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        pnlStenSaxPase.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+
+        btnSten.setIcon(new javax.swing.ImageIcon("C:\\Users\\elev\\Desktop\\Projekt\\Sten.png")); // NOI18N
+        btnSten.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnStenMouseClicked(evt);
+            }
+        });
+        btnSten.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnStenActionPerformed(evt);
+            }
+        });
+
+        btnSax.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Sax.png"))); // NOI18N
+        btnSax.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnSaxMouseClicked(evt);
+            }
+        });
+
+        btwPase.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Pase.png"))); // NOI18N
+        btwPase.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btwPaseMouseClicked(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Comic Sans MS", 1, 12)); // NOI18N
+        jLabel1.setText("Välj Sten, Sax eller Påse genom att trycka på motsvarande bild ");
+
+        jScrollPane1.setViewportView(txfResultat);
+
+        jScrollPane2.setViewportView(txfDatornsval);
+
+        jLabel3.setFont(new java.awt.Font("Comic Sans MS", 1, 12)); // NOI18N
+        jLabel3.setText("Datorn Valde:");
+
+        jLabel4.setFont(new java.awt.Font("Comic Sans MS", 1, 12)); // NOI18N
+        jLabel4.setText("Resultat:");
+
+        jLabel5.setFont(new java.awt.Font("Comic Sans MS", 1, 12)); // NOI18N
+        jLabel5.setText("Du valde:");
+
+        jScrollPane3.setViewportView(txfDittVal);
+
+        javax.swing.GroupLayout pnlStenSaxPaseLayout = new javax.swing.GroupLayout(pnlStenSaxPase);
+        pnlStenSaxPase.setLayout(pnlStenSaxPaseLayout);
+        pnlStenSaxPaseLayout.setHorizontalGroup(
+            pnlStenSaxPaseLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlStenSaxPaseLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(pnlStenSaxPaseLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pnlStenSaxPaseLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(pnlStenSaxPaseLayout.createSequentialGroup()
+                            .addComponent(btnSten, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGap(18, 18, 18)
+                            .addComponent(btnSax, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGap(18, 18, 18)
+                            .addComponent(btwPase, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pnlStenSaxPaseLayout.createSequentialGroup()
+                            .addGroup(pnlStenSaxPaseLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                .addComponent(jLabel3)
+                                .addComponent(jLabel4)
+                                .addComponent(jLabel5))
+                            .addGap(18, 18, 18)
+                            .addGroup(pnlStenSaxPaseLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 228, Short.MAX_VALUE)
+                                .addComponent(jScrollPane2)
+                                .addComponent(jScrollPane3)))))
+                .addGap(18, 18, 18))
+        );
+        pnlStenSaxPaseLayout.setVerticalGroup(
+            pnlStenSaxPaseLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlStenSaxPaseLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addGap(48, 48, 48)
+                .addGroup(pnlStenSaxPaseLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(btnSten, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnSax, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btwPase, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 36, Short.MAX_VALUE)
+                .addGroup(pnlStenSaxPaseLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel5))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(pnlStenSaxPaseLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(pnlStenSaxPaseLayout.createSequentialGroup()
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(pnlStenSaxPaseLayout.createSequentialGroup()
+                        .addComponent(jLabel3)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel4)))
+                .addContainerGap())
+        );
+
+        jLabel2.setFont(new java.awt.Font("Comic Sans MS", 1, 12)); // NOI18N
+        jLabel2.setText("Sten, Sax, Påse");
+
+        btnRestart.setFont(new java.awt.Font("Comic Sans MS", 1, 12)); // NOI18N
+        btnRestart.setText("Börja om");
+        btnRestart.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRestartActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(14, 14, 14)
+                        .addComponent(pnlStenSaxPase, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnRestart))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel2)))
+                .addContainerGap(333, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(8, 8, 8)
+                .addComponent(jLabel2)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(pnlStenSaxPase, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnRestart))
+                .addContainerGap(105, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnStenActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnStenActionPerformed
+
+      
+       
+    }//GEN-LAST:event_btnStenActionPerformed
+
+    private void btnStenMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnStenMouseClicked
+        //Gör ett tal mellan 1 och 3
+        java.util.Random slump = new java.util.Random();
+        int datornsTal = slump.nextInt(4); 
+        
+        
+        //Översätter slumptalet till Sten sax eller påse
+        String datornsVal = " ";
+        switch (datornsTal)
+                {
+                    case 1:
+                        datornsVal = "Sten";
+                        break;
+                    
+                    case 2:
+                        datornsVal = "Sax";
+                        break;
+                        
+                    case 3:
+                        datornsVal = "Påse";
+                        break;
+                }
+        
+
+        //Skriver ut ditt val i rutan "Ditt val"
+        txfDittVal.setText("Sten");
+
+        //Skriver ut datorns tal i rutan "Datorns val"
+        txfDatornsval.setText(datornsVal);
+        
+        //Visar vinnaren för Sten
+        if (datornsVal.equals("Sten"))
+        {
+            txfResultat.setText("Oavgjort");
+        }
+       
+        else if (datornsVal.equals("Sax"))
+        {
+            txfResultat.setText("Du vann");
+        }
+        else if (datornsVal.equals("Påse"))
+        {
+            txfResultat.setText("Du förlorade");
+        }
+        
+        
+        
+    }//GEN-LAST:event_btnStenMouseClicked
+
+    private void btnSaxMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnSaxMouseClicked
+        
+        //Gör ett tal mellan 1 och 3
+        java.util.Random slump = new java.util.Random();
+        int datornsTal = slump.nextInt(4); 
+        
+        
+        //Översätter slumptalet till Sten sax eller påse
+        String datornsVal = " ";
+        switch (datornsTal)
+                {
+                    case 1:
+                        datornsVal = "Sten";
+                        break;
+                    
+                    case 2:
+                        datornsVal = "Sax";
+                        break;
+                        
+                    case 3:
+                        datornsVal = "Påse";
+                        break;
+                }
+        
+        //Skriver ut ditt val i rutan "Ditt val"
+        txfDittVal.setText("Sax");
+        
+        //Skriver ut datorns tal i rutan "Datorns val"
+        txfDatornsval.setText(datornsVal);
+        
+        
+        //Visar vinnaren för Sax
+        if (datornsVal.equals("Sten"))
+        {
+            txfResultat.setText("Du förlorade");
+        }
+       
+        else if (datornsVal.equals("Sax"))
+        {
+            txfResultat.setText("Oavgjort");
+        }
+        else if (datornsVal.equals("Påse"))
+        {
+            txfResultat.setText("Du vann");
+        }
+    }//GEN-LAST:event_btnSaxMouseClicked
+
+    private void btwPaseMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btwPaseMouseClicked
+        
+        //Gör ett tal mellan 1 och 3
+        java.util.Random slump = new java.util.Random();
+        int datornsTal = slump.nextInt(4); 
+        
+        
+        //Översätter slumptalet till Sten sax eller påse
+        String datornsVal = " ";
+        switch (datornsTal)
+                {
+                    case 1:
+                        datornsVal = "Sten";
+                        break;
+                    
+                    case 2:
+                        datornsVal = "Sax";
+                        break;
+                        
+                    case 3:
+                        datornsVal = "Påse";
+                        break;
+                }
+        
+        //Skriver ut ditt val i rutan "Ditt val"
+        txfDittVal.setText("Påse");
+        
+        //Skriver ut datorns tal i rutan "Datorns val"
+        txfDatornsval.setText(datornsVal);
+        
+        
+        //Visar vinnaren för Påse
+        if (datornsVal.equals("Sten"))
+        {
+            txfResultat.setText("Du vann");
+        }
+       
+        else if (datornsVal.equals("Sax"))
+        {
+            txfResultat.setText("Du förlorade");
+        }
+        else if (datornsVal.equals("Påse"))
+        {
+            txfResultat.setText("Oavgjort");
+        }
+        
+    }//GEN-LAST:event_btwPaseMouseClicked
+
+    private void btnRestartActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRestartActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnRestartActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +377,21 @@ public class Skolprojekt extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnRestart;
+    private javax.swing.JButton btnSax;
+    private javax.swing.JButton btnSten;
+    private javax.swing.JButton btwPase;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JPanel pnlStenSaxPase;
+    private javax.swing.JTextPane txfDatornsval;
+    private javax.swing.JTextPane txfDittVal;
+    private javax.swing.JTextPane txfResultat;
     // End of variables declaration//GEN-END:variables
 }
